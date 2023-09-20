f83d12755384c2051c0cd1d2baeaa5ab8cce1a55
Reemaalt <reema.altj@gmail.com>
2023-09-20 23:24:37 +0300

    h

--- test.java
+++ test.java
@@ -1,7 +1,8 @@
  public class test
  {
- public static void main(String args[])
- { System.out.println("hi");
+ public static void main(String args[]){
+ int a=1 ;
+     System.out.println("hi");
  System.out.println("thank you reema(:");
  System.out.println("thank you reema(:");
 
